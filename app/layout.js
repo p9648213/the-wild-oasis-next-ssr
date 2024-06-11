549a8adddf8036c5d4add23048e79c6c0f5b2497
p9648213 <p9648213750912@gmail.com>
2024-06-11 07:59:11 +0700

    update home page

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,33 @@
-import Logo from "./_components/Logo";
-import Navigation from "./_components/Navigation";
+import Header from "./_components/Header";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 import "./_styles/globals.css";
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased ${josefin.className}`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
